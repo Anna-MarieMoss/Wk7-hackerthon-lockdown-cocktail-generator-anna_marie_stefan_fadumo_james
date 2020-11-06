fe9cc51d040e1a92e895836a1b55ca7ad8ae67a8
Stefan Kudev <stefan.yanchev.kudev@gmail.com>
2020-11-06 10:54:07 +0000

    Get random cocktail

---
+++ src/components/RandomizeButton/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+function RandomizeButton(props) {
+  const { setRandomCocktail } = props;
+
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    // fetch goes here if
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .then((response) => response.json())
+      .then((data) => console.log(data.drinks[0]))
+      .then((data) => setRandomCocktail(data));
+  }, [click]);
+
+  return <button onClick={() => setClick(!click)}>Get random cocktail!</button>;
+}
+
+export default RandomizeButton;
